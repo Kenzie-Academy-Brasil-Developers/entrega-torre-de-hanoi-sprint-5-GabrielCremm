eda359947cfc4da73977d8a3a09de4750c9c6da4
GabrielCremm <gcscremm@hotmail.com>
2021-08-04 18:46:57 -0300

    arrumando css

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const principal = document.getElementById("main");
 // COLUNAS
 const torreStart = document.createElement("div");
 torreStart.id="torreStart"
+torreStart.className="torre"
 const torreOffSet = document.createElement("div");
 torreOffSet.id="torreOffSet"
+torreOffSet.className="torre"
 const torreEnd = document.createElement("div");
 torreEnd.id="torreEnd"
+torreEnd.className="torre"
 // BARRAS
 const barra25 = document.createElement("div");
 const barra50 = document.createElement("div");
@@ -15,35 +18,16 @@ const barra100 = document.createElement("div");
 console.log(barra100);
 // ESTILOS
 //TORRE START
-torreStart.style.alignItems= "center";
-torreStart.style.justifyContent= "flex-end";
-torreStart.style.display= "flex";
-torreStart.style.flexDirection= "column";
-torreStart.style.width = "15px";
-torreStart.style.height = "15rem";
-torreStart.style.background = "black";
+
 
 principal.appendChild(torreStart);
 
 // TORRE OFF SET
-torreOffSet.style.alignItems= "center";
-torreOffSet.style.justifyContent= "flex-end";
-torreOffSet.style.display= "flex";
-torreOffSet.style.flexDirection= "column";
-torreOffSet.style.width = "15px";
-torreOffSet.style.height = "15rem";
-torreOffSet.style.background = "black";
 
 principal.appendChild(torreOffSet);
 
 //TORRE END
-torreEnd.style.alignItems= "center";
-torreEnd.style.justifyContent= "flex-end";
-torreEnd.style.display= "flex";
-torreEnd.style.flexDirection= "column";
-torreEnd.style.width = "15px";
-torreEnd.style.height = "15rem";
-torreEnd.style.background = "black";
+
 
 principal.appendChild(torreEnd);
 
